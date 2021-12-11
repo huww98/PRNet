a75be6899c0f51570910d896a61ac2f687851ba6
胡玮文 <sehuww@mail.scut.edu.cn>
2021-12-11 19:08:46 +0800

    extract common dataset logic

--- CelebA.py
+++ CelebA.py
@@ -15,35 +15,23 @@ from render import FaceRenderer
 
 logger = logging.getLogger(__name__)
 
-class CelebA(Dataset):
-    def __init__(self, path, expand_bb=1.6, cropped_res=256):
-        self.base_path = path
-        def f():
-            exts = ('.jpg', '.png')
-            for e in exts:
-                yield from glob.iglob(os.path.join(path, '*/*/live/*' + e))
-        self.image_path_list = list(tqdm(f(), desc='Enumerating images'))
+class FaceDataset(Dataset):
+    def __init__(self, face_images, expand_bb=1.6, cropped_res=256):
+        self.face_images = face_images
         self.expand_bb = expand_bb
         self.cropped_res = cropped_res
 
     def __len__(self):
-        return len(self.image_path_list)
+        return len(self.face_images)
 
     def __getitem__(self, index):
-        p = self.image_path_list[index]
-        image = imread(p)
+        image, bb, rel_path = self.face_images[index]
         image = image/255.
         h, w, c = image.shape
-        p_bb = os.path.splitext(p)[0] + '_BB.txt'
-        try:
-            # [[x0, y0], [w, h]]
-            bb = np.array([float(b) for b in open(p_bb).read().split()[:4]]).reshape(2,2)
-            bb /= 224.
-            bb[:,0] *= w
-            bb[:,1] *= h
+        if bb is not None:
             bb[0] -= bb[1] * (self.expand_bb - 1) / 2
             bb[1] *= self.expand_bb
-        except IndexError:
+        else:
             # in case we have invalid bonding box info
             bb = np.array([[0.,0.],[w,h]])
         scale = math.sqrt(self.cropped_res ** 2 / np.prod(bb[1]))
@@ -52,12 +40,41 @@ class CelebA(Dataset):
             translation=self.cropped_res / 2 - (bb[0] + bb[1] / 2) * scale,
         )
         image = warp(image, tform.inverse, output_shape=(self.cropped_res, self.cropped_res))
-        return image, os.path.splitext(os.path.relpath(p, self.base_path))[0]
+        return image, os.path.splitext(rel_path)[0]
 
+class CelebA:
+    def __init__(self, path):
+        self.base_path = path
+        def f():
+            exts = ('.jpg', '.png')
+            for e in exts:
+                yield from glob.iglob(os.path.join(path, '*/*/live/*' + e))
+        self.image_path_list = list(tqdm(f(), desc='Enumerating images'))
+
+    def __len__(self):
+        return len(self.image_path_list)
+
+    def __getitem__(self, index):
+        p = self.image_path_list[index]
+        image = imread(p)
+        h, w, c = image.shape
+        p_bb = os.path.splitext(p)[0] + '_BB.txt'
+        try:
+            # [[x0, y0], [w, h]]
+            bb = np.array([float(b) for b in open(p_bb).read().split()[:4]]).reshape(2,2)
+            bb /= 224.
+            bb[:,0] *= w
+            bb[:,1] *= h
+        except IndexError:
+            # in case we have invalid bonding box info
+            bb = None
+        return image, bb, os.path.relpath(p, self.base_path)
 
 def main(args):
     dataloader = DataLoader(
-        CelebA(args.celebA_path),
+        FaceDataset(
+            CelebA(args.celebA_path),
+        ),
         batch_size=64,
         num_workers=32,
     )
